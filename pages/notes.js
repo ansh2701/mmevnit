6d81000c90d50eeb7d80633d698368af74d673f2
ansh2701 <agrawalansh2701@gmail.com>
2021-08-06 11:56:14 +0530

    first commit

--- pages/notes.js
+++ pages/notes.js
@@ -1,17 +1,10 @@
+import Layout from "../components/Layout";
+
 const notes = () => {
   return (
-    <div>
-      <h1>Notes</h1>
-      <div className={styles.card}>
-        <div className={styles.tags}>
-          <span>Subject</span>
-          <span>Deadline</span>
-        </div>
-        <div className={styles.notice}>
-          <p>Notice</p>
-        </div>
-      </div>
-    </div>
+    <Layout>
+      <h1>Nothing to show</h1>
+    </Layout>
   );
 };
 
