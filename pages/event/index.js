63b5d2d86b0e2ecdac10f1883471cb1fcea25266
ansh2701 <agrawalansh2701@gmail.com>
2021-10-01 17:17:40 +0530

    event

--- pages/event/index.js
+++ pages/event/index.js
@@ -1,22 +1,116 @@
 import Image from "next/image";
+import Link from "next/link";
 import Layout from "../../components/Layout";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import styles from "../../styles/Event.module.css";
 
 const index = () => {
   return (
     <Layout>
       <div className={styles.back}>
-        <div className={styles.carousel}></div>
-        <div className={styles.container}>
-          <div className={styles.heading}>
-            <h2>Upcoming Events</h2>
-            <span>See all</span>
+        <div className={styles.hero}>
+          <Carousel
+            showArrows={false}
+            autoPlay={true}
+            showThumbs={false}
+            infiniteLoop={true}
+          >
+            <div className={styles.hero}>
+              <div className={styles.image}>
+                <Image src="/concert.jpg" layout="fill" alt="concert" />
+              </div>
+            </div>
+            <div className={styles.image}>
+              <Image
+                src="https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_960_720.jpg"
+                layout="fill"
+                alt="concert"
+              />
+            </div>
+            <div className={styles.image}>
+              <Image
+                src="https://cdn.pixabay.com/photo/2014/05/03/01/02/stage-336695_960_720.jpg"
+                layout="fill"
+                alt="concert"
+              />
+            </div>
+          </Carousel>
+          <div className={styles.hero_text}>
+            <h1>EVENTS</h1>
           </div>
-          <div className={styles.contentWrapper}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        </div>
+        <div className={styles.box}>
+          <div className={styles.gallery}>
+            <Link href="/event/gallery">
+              <a>
+                <div className={styles.gallery_btn}>
+                  <span>Photo Gallery</span>
+                  <div className={styles.overlap}>
+                    <ul>
+                      <li>
+                        <Image
+                          src="https://cdn.pixabay.com/photo/2016/11/22/19/15/hand-1850120_960_720.jpg"
+                          height={120}
+                          width={120}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <Image
+                          src="https://cdn.pixabay.com/photo/2016/11/22/19/15/hand-1850120_960_720.jpg"
+                          height={120}
+                          width={120}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <Image
+                          src="https://cdn.pixabay.com/photo/2016/11/22/19/15/hand-1850120_960_720.jpg"
+                          height={120}
+                          width={120}
+                          alt=""
+                        />
+                      </li>
+                      <li>
+                        <Image
+                          src="https://cdn.pixabay.com/photo/2016/11/22/19/15/hand-1850120_960_720.jpg"
+                          height={120}
+                          width={120}
+                          alt=""
+                        />
+                      </li>
+                    </ul>
+                    <p> +10 Images</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.heading}>
+              <h2>Upcoming Events</h2>
+              <span>See all</span>
+            </div>
+            <div className={styles.contentWrapper}>
+              {/* <Carousel
+              showArrows={false}
+              autoPlay={true}
+              showThumbs={false}
+              infiniteLoop={true}
+            > */}
+              <Card />
+              <Card />
+            </div>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.heading}>
+              <h2>Previous Events</h2>
+              <span>See all</span>
+            </div>
+            <div className={styles.contentWrapper}>
+              <Card />
+            </div>
           </div>
         </div>
       </div>
